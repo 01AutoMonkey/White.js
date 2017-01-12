3f756ad57ae0fa16cd3d72ada27d90294f578a80
Brynjar Harðarson <brynjar@gmail.com>
2017-01-12 20:14:26 +0000

    docReady for when you include the script in the head.

--- src/white.js
+++ src/white.js
@@ -101,12 +101,12 @@ var White = {
       img.src = img.dataset.src;
     }
   },
-  getCanvas: function(w, h) {
+  /*getCanvas: function(w, h) {
     var c = document.createElement('canvas');
     c.width = w;
     c.height = h;
     return c;
-  },
+  },*/
   filterImage: function(image, w, h, c) {
     return this.filter(this.getPixels(image, w, h, c));
   },
@@ -137,9 +137,9 @@ var White = {
       pixels.data[b] += this.brightness; // b
 
       // Contrast
-      pixels.data[r] = this.factor * (pixels.data[i] - 128) + 128; // r
-      pixels.data[g] = this.factor * (pixels.data[i+1] - 128) + 128; // g
-      pixels.data[b] = this.factor * (pixels.data[i+2] - 128) + 128; // b
+      pixels.data[r] = this.factor * (pixels.data[r] - 128) + 128; // r
+      pixels.data[g] = this.factor * (pixels.data[g] - 128) + 128; // g
+      pixels.data[b] = this.factor * (pixels.data[b] - 128) + 128; // b
 
       // Black Threshold
       if (pixels.data[r] < this.black_threshold) { // r
@@ -192,6 +192,82 @@ var QueryString = function () {
   return query_string;
 }();
 
+
+(function(funcName, baseObj) {
+    "use strict";
+    // The public function name defaults to window.docReady
+    // but you can modify the last line of this function to pass in a different object or method name
+    // if you want to put them in a different namespace and those will be used instead of 
+    // window.docReady(...)
+    funcName = funcName || "docReady";
+    baseObj = baseObj || window;
+    var readyList = [];
+    var readyFired = false;
+    var readyEventHandlersInstalled = false;
+    
+    // call this when the document is ready
+    // this function protects itself against being called more than once
+    function ready() {
+        if (!readyFired) {
+            // this must be set to true before we start calling callbacks
+            readyFired = true;
+            for (var i = 0; i < readyList.length; i++) {
+                // if a callback here happens to add new ready handlers,
+                // the docReady() function will see that it already fired
+                // and will schedule the callback to run right after
+                // this event loop finishes so all handlers will still execute
+                // in order and no new ones will be added to the readyList
+                // while we are processing the list
+                readyList[i].fn.call(window, readyList[i].ctx);
+            }
+            // allow any closures held by these functions to free
+            readyList = [];
+        }
+    }
+    
+    function readyStateChange() {
+        if ( document.readyState === "complete" ) {
+            ready();
+        }
+    }
+    
+    // This is the one public interface
+    // docReady(fn, context);
+    // the context argument is optional - if present, it will be passed
+    // as an argument to the callback
+    baseObj[funcName] = function(callback, context) {
+        // if ready has already fired, then just schedule the callback
+        // to fire asynchronously, but right away
+        if (readyFired) {
+            setTimeout(function() {callback(context);}, 1);
+            return;
+        } else {
+            // add the function and context to the list
+            readyList.push({fn: callback, ctx: context});
+        }
+        // if document already ready to go, schedule the ready function to run
+        // IE only safe when readyState is "complete", others safe when readyState is "interactive"
+        if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
+            setTimeout(ready, 1);
+        } else if (!readyEventHandlersInstalled) {
+            // otherwise if we don't have event handlers installed, install them
+            if (document.addEventListener) {
+                // first choice is DOMContentLoaded event
+                document.addEventListener("DOMContentLoaded", ready, false);
+                // backup is window load event
+                window.addEventListener("load", ready, false);
+            } else {
+                // must be IE
+                document.attachEvent("onreadystatechange", readyStateChange);
+                window.attachEvent("onload", ready);
+            }
+            readyEventHandlersInstalled = true;
+        }
+    }
+})("docReady", window);
+// modify this previous line to pass in your own method name 
+// and object for the method to be attached to
+
 if (QueryString.proxy) White.proxy = QueryString.proxy;
 if (QueryString.selector) White.selector = QueryString.selector;
 if (QueryString.brightness) White.brightness = parseInt(QueryString.brightness);
@@ -199,4 +275,6 @@ if (QueryString.contrast) White.contrast = parseInt(QueryString.contrast);
 if (QueryString.black_threshold) White.black_threshold = parseInt(QueryString.black_threshold);
 if (QueryString.white_threshold) White.white_threshold = parseInt(QueryString.white_threshold);
 if (QueryString.selector) White.selector = QueryString.selector;
-if (QueryString.run === "true") White.run();
+if (QueryString.run === "true") docReady(function() {
+  White.run();
+});
